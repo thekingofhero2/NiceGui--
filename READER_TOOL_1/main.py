c73942e05ba7055203bb4eaabb8ac45f9c25762d
thekingofhero2 <thekingofhero@live.com>
2025-03-18 08:51:47 +0800

    t

--- READER_TOOL_1/main.py
+++ READER_TOOL_1/main.py
@@ -7,28 +7,35 @@ import time
 import re 
 from nicegui.events import KeyEventArguments,KeyboardModifiers,GenericEventArguments
 from settings import *
+import os 
 
 class MainTool:
     def __init__(self):
-        self.dict_m =  {'video_file':r"D:\01nipon\逃避虽可耻但有用 逃げるは恥だが役に立つ (2016)\01.mp4"
-          ,"subtitle_file":r"D:\01nipon\逃避虽可耻但有用 逃げるは恥だが役に立つ (2016)\逃避虽可耻但有用.逃げるは恥だが役に立つ.S01E01.2016.1080p.KKTV.WEB-DL.H264.AAC.chs&jpn.ass"
+        self.dict_m =  {'video_file':""#r"C:\Users\lenovo\Videos\bilibili\output\IT商务日语-面试-【短评】面接的重点.mp4"
+          ,"subtitle_file":""#r"C:\Users\lenovo\Videos\bilibili\output\IT商务日语-面试-【短评】面接的重点.srt"
+          ,"subtitle_file_z":""#r"C:\Users\lenovo\Videos\bilibili\output\IT商务日语-面试-【短评】面接的重点.ja.srt"
           ,"end_long":999999999
           ,"global_index":0
           ,"current_cn":""
           ,"current_other":""
-          ,"show_current":False}
+          ,"show_current":False
+          ,"last_directory":"~"}
 
     async def pick_file(self,type="v") -> None:
         """
         :type :"v" or "s" ，v代表选取video，s代表选取字幕文件
         """
-        result = await local_file_picker('~', multiple=False)
-        print(result)
-        if type == 'v':
-            self.dict_m['video_file'] = result[0]
-        elif type == 's':
-            self.dict_m['subtitle_file'] = result[0]
-        self.show_video.refresh()
+        print(self.dict_m['last_directory'])
+        result = await local_file_picker(self.dict_m['last_directory'], multiple=False)
+        if result is not None:
+            self.dict_m['last_directory'] = os.path.dirname(result[0])
+            if type == 'v':
+                self.dict_m['video_file'] = result[0]
+            elif type == 's':
+                self.dict_m['subtitle_file'] = result[0]
+            elif type == 's_z':
+                self.dict_m['subtitle_file_z'] = result[0]
+            self.show_video.refresh()
         #ui.notify(f'You chose {result}')
 
     
@@ -75,6 +82,7 @@ class MainTool:
             self.play_next()
         if e.args['code'] == 'Enter' :
             self.play_re()
+            
             self.dict_m["show_current"] = True
             
         # if e.key in ('r','R') and KeyboardModifiers.ctrl :
@@ -84,9 +92,12 @@ class MainTool:
         # if e.key in ('n','N') and KeyboardModifiers.ctrl :
         #     self.play_next()
     def show_sub_title(self,):
-        #with ui.separator():
-        ui.input().classes("w-full h-[60px]").bind_value_from(self.dict_m,"current_cn").bind_visibility_from(self.dict_m,"show_current")
-        ui.input().classes("w-full h-[60px]").bind_value_from(self.dict_m,"current_other").bind_visibility_from(self.dict_m,"show_current")
+        with ui.column().classes("w-full"):
+            ui.label("外文原文：")
+            ui.input().classes("w-full h-[60px]").bind_value_from(self.dict_m,"current_other").bind_visibility_from(self.dict_m,"show_current")
+            ui.label("中文相关说明：")
+            ui.input().classes("w-full h-[60px]").bind_value_from(self.dict_m,"current_cn").bind_visibility_from(self.dict_m,"show_current")
+        
     def play_next(self):
         "播放下一曲"
         start ,long_s = self.p[self.dict_m['global_index']][0],self.p[self.dict_m['global_index']][1]
@@ -112,46 +123,66 @@ class MainTool:
             self.dict_m['global_index'] -= 1
         self.play_next()
     def part_play(self,):
-        if len(self.dict_m['subtitle_file']) > 0:
-            subs = pysubs2.load(self.dict_m['subtitle_file'], encoding="utf-8")
-            def lang_type(txt):
-                jap = re.compile(r'[\u3040-\u309F\u30A0-\u30FF\uAC00-\uD7A3]')  # \uAC00-\uD7A3为匹配韩文的，其余为日文
-                if jap.search(txt):
-                    return 'jap'
-                return 'cn'
-            res_all = {}
-            for index_i,line in enumerate(subs):
-                cn,other = (None,None)
-                s = line.text.split("\\N")
-                if len(s) > 1:
-                    #说明 中文、外文都在一条字幕上显示（无论是这个时代 还是人心 \\N 時代も 人の心も）
-                    cn,other = (s[0][s[0].find("}")+1:],s[1][s[1].find("}")+1:])
-                else:
-                    #说明 中文、外文都不一条字幕上显示，需要将两个starttime相同的字幕关联起来。
-                    s = s[0]
-                    if lang_type(s) == 'cn':
-                        cn = s
-                    else:
-                        other = s
-                if line.start not in res_all.keys():
-                    res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,cn,other)
-                else:
-                    if cn is None:
-                        res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,res_all[line.start][2],other)
-                    elif other is None:
-                        res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,cn,res_all[line.start][3])
+        if len(self.dict_m['subtitle_file']) > 0 and len(self.dict_m['subtitle_file_z']) > 0 :
+            subs_org = pysubs2.load(self.dict_m['subtitle_file'], encoding="utf-8")
+            subs_z = pysubs2.load(self.dict_m['subtitle_file_z'], encoding="utf-8")
+            if len(subs_org) != len(subs_z):
+                ui.notify("字幕文件不对！")
+                return
+            else:
+                res_all = {}
+                for index_i,line in enumerate(subs_org):
+                    cn,other = (subs_z[index_i].text,line.text)
+                    if line.start not in res_all.keys():
+                        res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,cn,other)
+                
                
             res_all2 = {}
             for index_i ,item in enumerate(res_all.items()):
+                print(item[1])
                 res_all2[index_i]=item[1]
-
             return res_all2
+        # if len(self.dict_m['subtitle_file']) > 0:
+        #     subs = pysubs2.load(self.dict_m['subtitle_file'], encoding="utf-8")
+        #     def lang_type(txt):
+        #         jap = re.compile(r'[\u3040-\u309F\u30A0-\u30FF\uAC00-\uD7A3]')  # \uAC00-\uD7A3为匹配韩文的，其余为日文
+        #         if jap.search(txt):
+        #             return 'jap'
+        #         return 'cn'
+        #     res_all = {}
+        #     for index_i,line in enumerate(subs):
+        #         cn,other = (None,None)
+        #         s = line.text.split("\\N")
+        #         if len(s) > 1:
+        #             #说明 中文、外文都在一条字幕上显示（无论是这个时代 还是人心 \\N 時代も 人の心も）
+        #             cn,other = (s[0][s[0].find("}")+1:],s[1][s[1].find("}")+1:])
+        #         else:
+        #             #说明 中文、外文都不一条字幕上显示，需要将两个starttime相同的字幕关联起来。
+        #             s = s[0]
+        #             if lang_type(s) == 'cn':
+        #                 cn = s
+        #             else:
+        #                 other = s
+        #         if line.start not in res_all.keys():
+        #             res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,cn,other)
+        #         else:
+        #             if cn is None:
+        #                 res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,res_all[line.start][2],other)
+        #             elif other is None:
+        #                 res_all[line.start] = (line.start/1000,line.end/1000 - line.start/1000,cn,res_all[line.start][3])
+               
+        #     res_all2 = {}
+        #     for index_i ,item in enumerate(res_all.items()):
+        #         res_all2[index_i]=item[1]
+
+        #     return res_all2
 @ui.page('/')
 def index():
     maintool = MainTool()
-    # with ui.row():
-    #     ui.button('选择视频文件', on_click=lambda x:pick_file("v"), icon='folder')
-    #     ui.button('选择字幕文件', on_click=lambda x:pick_file("s"), icon='folder')
+    with ui.row():
+        ui.button('选择视频文件', on_click=lambda x:maintool.pick_file("v"), icon='folder')
+        ui.button('选择外文字幕文件', on_click=lambda x:maintool.pick_file("s"), icon='folder')
+        ui.button('选择中文字幕文件', on_click=lambda x:maintool.pick_file("s_z"), icon='folder')
     
    
         
@@ -159,4 +190,5 @@ def index():
     #keyboard = ui.keyboard(on_key=maintool.handle_key)
     maintool.show_video()
 
-ui.run(native=True,reload=True,window_size=(320, 720))
+#ui.run(host='0.0.0.0',native=True,reload=True,window_size=(320, 720))
+ui.run(native=True,reload=True,window_size=(640, 720))
